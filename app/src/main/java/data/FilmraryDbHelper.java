7d87ee5314be4d71940678c4c28be2d94cda2c63
AlexaEgorova <egorova.a007@gmail.com>
2020-04-23 01:22:23 +0300

    fill tables' sql

--- app/src/main/java/data/FilmraryDbHelper.java
+++ app/src/main/java/data/FilmraryDbHelper.java
@@ -5,10 +5,11 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+
 public class FilmraryDbHelper extends SQLiteOpenHelper {
 
     public static final String LOG_TAG = FilmraryDbHelper.class.getSimpleName();
-    public static final String DATABASE_NAME = "films.db";
+    public static final String DATABASE_NAME = "filmrary.db";
     private static final int DATABASE_VERSION = 1;
 
     public FilmraryDbHelper(Context context) {
@@ -17,20 +18,77 @@ public class FilmraryDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String SQL_CREATE_MOVIES_TABLE = "CREATE TABLE " + FilmsContract.Films.TABLE_NAME + "("
+        String SQL_CREATE_FILMS_TABLE = "CREATE TABLE " + FilmsContract.Films.TABLE_NAME + "("
                 + FilmsContract.Films._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + FilmsContract.Films.COLUMN_NAME + " TEXT NOT NULL, "
                 + FilmsContract.Films.COLUMN_YEAR + " INTEGER NOT NULL, "
-                + FilmsContract.Films.COLUMN_COUNTRY + " TEXT NOT NULL, "
-                + FilmsContract.Films.COLUMN_GANRE + " TEXT NOT NULL, "
-                + FilmsContract.Films.COLUMN_ACTOR + " TEXT NOT NULL, "
-                + FilmsContract.Films.COLUMN_PRODUCER + " TEXT NOT NULL, "
-                + FilmsContract.Films.COLUMN_IMDB + " DOUBLE NOT NULL, "
-                + FilmsContract.Films.COLUMN_KINOPOISK + " DOUBLE NOT NULL, "
-                + FilmsContract.Films.COLUMN_WANT + " INT NOT NULL, "
-                + FilmsContract.Films.COLUMN_DESCRIPTION + " TEXT NOT NULL);";
+                + FilmsContract.Films.COLUMN_COUNTRY + " TEXT, "
+                + FilmsContract.Films.COLUMN_AGE + " INT, "
+                + FilmsContract.Films.COLUMN_GANRE + " TEXT, "
+                + FilmsContract.Films.COLUMN_ACTOR + " TEXT, "
+                + FilmsContract.Films.COLUMN_PRODUCER + " TEXT, "
+                + FilmsContract.Films.COLUMN_IMDB + " DOUBLE, "
+                + FilmsContract.Films.COLUMN_KINOPOISK + " DOUBLE, "
+                + FilmsContract.Films.COLUMN_WANT + " INT, "
+                + FilmsContract.Films.COLUMN_LINK + " TEXT, "
+                + FilmsContract.Films.COLUMN_DESCRIPTION + " TEXT)";
 
-        db.execSQL(SQL_CREATE_MOVIES_TABLE);
+        String SQL_CREATE_ACTORS_TABLE = "CREATE TABLE " + ActorsContract.Actors.TABLE_NAME + "("
+                + ActorsContract.Actors._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + ActorsContract.Actors.COLUMN_NAME + " TEXT NOT NULL, "
+                + ActorsContract.Actors.COLUMN_SURNAME + " TEXT NOT NULL, "
+                + ActorsContract.Actors.COLUMN_NICKNAME + " TEXT, "
+                + ActorsContract.Actors.COLUMN_BIRTHDAY + " DATETIME, "
+                + ActorsContract.Actors.COLUMN_INFO + " TEXT" + ")";
+
+        String SQL_CREATE_ACTOR_FILM_TABLE = "CREATE TABLE " + ActorFilmContract.ActorFilm.TABLE_NAME + "("
+                + ActorFilmContract.ActorFilm._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + ActorFilmContract.ActorFilm.COLUMN_ACTOR_ID + " INTEGER NOT NULL, "
+                + ActorFilmContract.ActorFilm.COLUMN_FILM_ID + " INTEGER NOT NULL" + ")";
+
+        String SQL_CREATE_COUNTRIES_TABLE = "CREATE TABLE " + CountriesContract.Countries.TABLE_NAME + "("
+                + CountriesContract.Countries._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + CountriesContract.Countries.COLUMN_NAME  + " TEXT NOT NULL" + ")";
+
+        String SQL_CREATE_COUNTRY_FILM_TABLE = "CREATE TABLE " + CountryFilmContract.CountryFilm.TABLE_NAME + "("
+                + CountryFilmContract.CountryFilm._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + CountryFilmContract.CountryFilm.COLUMN_COUNTRY_ID + " INTEGER NOT NULL, "
+                + CountryFilmContract.CountryFilm.COLUMN_FILM_ID + " INTEGER NOT NULL" + ")";
+
+        String SQL_CREATE_GANRES_TABLE = "CREATE TABLE " + GanresContract.Ganres.TABLE_NAME + "("
+                + GanresContract.Ganres._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + GanresContract.Ganres.COLUMN_NAME + " TEXT NOT NULL, "
+                + GanresContract.Ganres.COLUMN_INFO + " TEXT" + ")";
+
+        String SQL_CREATE_GANRE_FILM_TABLE = "CREATE TABLE " + GanreFilmContract.GanreFilm.TABLE_NAME + "("
+                + GanreFilmContract.GanreFilm._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + GanreFilmContract.GanreFilm.COLUMN_GANRE_ID + " INTEGER NOT NULL, "
+                + GanreFilmContract.GanreFilm.COLUMN_FILM_ID + " INTEGER NOT NULL" + ")";
+
+        String SQL_CREATE_PRODUCERS_TABLE = "CREATE TABLE " + ProducersContract.Producers.TABLE_NAME + "("
+                + ProducersContract.Producers._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + ProducersContract.Producers.COLUMN_NAME + " TEXT NOT NULL, "
+                + ProducersContract.Producers.COLUMN_SURNAME + " TEXT NOT NULL, "
+                + ProducersContract.Producers.COLUMN_INFO + " TEXT" + ")";
+
+        String SQL_CREATE_PRODUCER_FILM_TABLE = "CREATE TABLE " + ProducerFilmContract.ProducerFilm.TABLE_NAME + "("
+                + ProducerFilmContract.ProducerFilm._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + ProducerFilmContract.ProducerFilm.COLUMN_PRODUCER_ID + " INTEGER NOT NULL, "
+                + ProducerFilmContract.ProducerFilm.COLUMN_FILM_ID + " INTEGER NOT NULL" + ")";
+
+        String SQL_CREATE_WANT_TO_WATCH_TABLE = "CREATE TABLE " + WantToWatchContract.WantToWatch.TABLE_NAME + "("
+                + WantToWatchContract.WantToWatch._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + WantToWatchContract.WantToWatch.COLUMN_FILM_ID + " INTEGER NOT NULL, "
+                + WantToWatchContract.WantToWatch.COLUMN_ADD_DATE + " DATETIME" + ")";
+
+        String SQL_CREATE_WATCHED_TABLE = "CREATE TABLE " + WatchedContract.Watched.TABLE_NAME + "("
+                + WatchedContract.Watched._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + WatchedContract.Watched.COLUMN_FILM_ID + " INTEGER NOT NULL, "
+                + WatchedContract.Watched.COLUMN_DATE + " DATETIME, "
+                + WatchedContract.Watched.COLUMN_RATE + " INTEGER, "
+                + WatchedContract.Watched.COLUMN_OPINION + " TEXT" + ")";
+
+        db.execSQL(SQL_CREATE_FILMS_TABLE);
     }
 
     @Override
