558ef55c0bd0477fafba9a670dda1c0b12092e1c
glkv <glkv@protonmail.ch>
2020-04-27 04:44:22 +0300

    Вынес enum

--- app/src/main/java/com/example/filmoteka/EditorActivity.java
+++ app/src/main/java/com/example/filmoteka/EditorActivity.java
@@ -22,25 +22,6 @@ import data.FilmsContract;
 import java.util.ArrayList;
 import java.util.Calendar;
 
-enum CODE_RETURN {
-    COUNTRY(1),
-    GENRE(2),
-    PRODUCER(3),
-    ACTOR(4),
-    WANT_TO_WATCH(5),
-    WATCHED(6);
-
-    private final int value;
-
-    CODE_RETURN(int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return value;
-    }
-}
-
 public class EditorActivity extends AppCompatActivity {
 
     private EditText fNameEditText;
